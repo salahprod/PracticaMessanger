fbaa1aa4d2448598a6afd34dac1d48c49bda2e51
Артём <artemcesevik655@gmail.com>
2025-11-12 14:55:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidmessage1/RegisterActivity.java
@@ -0,0 +1,63 @@
+package com.example.androidmessage1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.androidmessage1.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public void goToLoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0); // Убирает анимацию
+        finish(); // опционально
+    }
+    private ActivityRegisterBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (binding.loginText2.getText().toString().isEmpty() || binding.passwordText2.getText().toString().isEmpty()
+                        || binding.emailText2.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.emailText2.getText().toString(), binding.passwordText2.getText().toString());
+                    HashMap<String,String> userInfo = new HashMap<>();
+                    userInfo.put("login", binding.loginText2.getText().toString());
+                    userInfo.put("email", binding.emailText2.getText().toString());
+                    FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(userInfo);
+                    startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+
+                }
+            }
+        });
+    }
+}
